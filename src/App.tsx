3a42072678ef70d40daa4b723360f582732b4b76
Ronada <basharonada@gmail.com>
2025-07-18 23:11:05 +0200

    Slider without lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import SliderWithLazyLoading from "./examples/lazy-loading/slider-with-lazy-loading";
+import Slider from "./examples/lazy-loading/slider";
+//import SliderWithLazyLoading from "./examples/lazy-loading/slider-with-lazy-loading";
 
 function App() {
   return (
     <div className="App">
-      <SliderWithLazyLoading />
+      {/* <SliderWithLazyLoading /> */}
+      <Slider />
     </div>
   );
 }
--- src/examples/lazy-loading/slider.tsx
+++ src/examples/lazy-loading/slider.tsx
@@ -1,5 +1,50 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { slides } from "../../data/slider";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
+
 const Slider = () => {
-  return <div>slider ...</div>;
+  return (
+    <div className="z-10 relative">
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        autoplay
+        scrollbar={{ draggable: true }}
+      >
+        {slides.map((slide, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div
+                className="h-screen bg-cover bg-center transition-all duration-700"
+                style={{
+                  backgroundImage: `url(${slide.image})`,
+                }}
+              >
+                <div className="relative max-w-6xl h-screen mx-auto">
+                  <div className="absolute bg-[#00000094] text-white max-w-full mx-6 md:mx-0 md:max-w-[500px] p-8 left-0 bottom-6 md:bottom-auto md:top-1/2 -mt-[22px]">
+                    <h1 className="text-xl font-urbanist font-bold mb-8">
+                      {slide.title}
+                    </h1>
+                    <hr className="border-t-2 border-[#18776A] w-[50px] mb-6" />
+                    <p className="font-urbanist font-medium">{slide.text}</p>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
 };
 
 export default Slider;
