0c7de0d568e37131ed8d8a9f821d0fdeec4114d6
Ronada <basharonada@gmail.com>
2025-07-24 10:11:53 +0200

    clean the master branch from a wrong commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import React, { Suspense } from "react";
-import Button from "@mui/material/Button";
-
-const LazyModal = React.lazy(() => import("./components/lazy-modal"));
+import React from "react";
 
 function App() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  return (
-    <div className="App">
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Suspense fallback={null}>
-        {open && <LazyModal handleClose={handleClose} isOpen={open} />}
-      </Suspense>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
