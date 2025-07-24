33b635c5a0e4c637240bb3e71b3dbbe956bb4872
Ronada <basharonada@gmail.com>
2025-07-24 09:47:29 +0200

    lazy loading modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
+import Button from "@mui/material/Button";
+
+const LazyModal = React.lazy(() => import("./components/lazy-modal"));
 
 function App() {
-  return <div className="App"></div>;
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <div className="App">
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Suspense fallback={null}>
+        {open && <LazyModal handleClose={handleClose} isOpen={open} />}
+      </Suspense>
+    </div>
+  );
 }
 
 export default App;
